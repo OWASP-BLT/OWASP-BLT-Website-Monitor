81a27c50ff57de8ea3054a6551a74cdc86642064
Riddhish1 <bonderiddhish@gmail.com>
2025-12-23 13:20:11 +0530

    Refactor code structure for improved readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: [
     "./index.html",
-    "./.github/workflows/website-monitor.yml"
+    "./src/**/*.css"
+  ],
+  safelist: [
+    'bg-green-100', 'text-green-800', 'bg-red-100', 'text-red-800',
+    'bg-blue-100', 'text-blue-800', 'bg-yellow-100', 'text-yellow-800',
+    'bg-blue-600', 'hover:bg-blue-700'
   ],
   theme: {
     extend: {
